74e1765ef326088b513f26fce54435d4ae21e357
xujiayuanqqq <1171242067@qq.com>
2018-06-17 01:02:47 +0800

    基本完成

--- jiketime/pages/index/index.js
+++ jiketime/pages/index/index.js
@@ -6,35 +6,52 @@ Page({
   data: {
     currentTab: 0,
     num: 1,
-    pages: [
-      {id: 1, picture: "../image/avatar1.png", name: "王天一", introduce: "工学博士，副教授", instr: "机器学习40讲", content: "帮你打通机器学习的任督二脉", price: "￥45/40期", learn: "7510 人与你共同学习"},
-      {id: 2, picture: "../image/avatar2.png", name: "蔡能", introduce: "原网易游戏引擎架构师，资深游...", instr: "从0开始学游戏开发", content: "你的游戏开发入门第一课", price: "￥68/36期", learn: "3157 人与你共同学习"},
-      {id: 3, picture: "../image/avatar3.png", name: "杨晓峰", introduce: "Oracle首席工程师", instr: "Java核心技术36讲", content: "Oracle首席工程师带你修炼Java内功", price: "￥68/36期", learn: "15756 人与你共同学习"},
-      {id: 4, picture: "../image/avatar4.png", name: "李运华", introduce: "资深技术专家", instr: "从0开始学架构", content: "资深技术专家的实战架构心法", price: "￥99/50期", learn: "20706 人与你共同学习"},
-    ]
+    pages: []
   },
   swiperTab: function(e) {
     // console.log(e);
-    let number = e.detail.current + 1;    
+    let number = e.detail.current + 1;  
     this.setData({
       currentTab: e.detail.current,
       num: number
     });
   },
-  readMore: function() {
-    // console.log(this);
+  goTo: function(e) {
+    var index = 20;
     wx.navigateTo({
-      url: '../jiketime/items/detail/detail',
-      success: function(res){
-        // success
-        // console.log(res);
-      },
-      fail: function() {
-        // fail
-      },
-      complete: function() {
-        // complete
-      }
+      url: '../jiketime/items/detail/detail?index=' + index,
+    })
+  },
+  goFor: function(e) {
+    var index = 21;
+    wx.navigateTo({
+      url: '../jiketime/items/detail/detail?index=' + index,
+    })
+  },
+  click: function(e) {
+    var index = 0;
+    wx.navigateTo({
+      url: '../jiketime/items/detail/detail?index=' + index,
+    })
+  },
+  clickTo: function(e) {
+    var index = 0;
+    wx.navigateTo({
+      url: '../jiketime/items/video/video?index=' + index,
+    })
+  },
+  readMore: function(e) {
+    var index = e.currentTarget.dataset.index;
+    if(index == 2 || index == 3) {
+      index = index + 1;
+    }
+    wx.navigateTo({
+      url: '../jiketime/items/detail/detail?index=' + index,
+    })
+  },
+  onLoad: function(options) {
+    this.setData({
+      pages: app.globalData.pages
     })
   }
 })
